7eeaeedb708d411c773a0ba99fb19f28a2cc5ac3
daoauth <57783762+daoauth@users.noreply.github.com>
2024-05-22 14:50:50 +0900

    Update configSample.ts

--- src/component/zkLogin/configSample.ts
+++ src/component/zkLogin/configSample.ts
@@ -1,4 +1,4 @@
 export const ClientId = '';
 export const Redirect = '';
-export const Rpc = 'https://prover-dev.mystenlabs.com/v1';
+export const Rpc = 'https://fullnode.testnet.sui.io';
 export const prover = 'https://prover-dev.mystenlabs.com/v1';
